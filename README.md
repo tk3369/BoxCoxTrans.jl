c22849170e44ec4a2970200daa71e3b362352eda
Tom Kwong <tk3369@gmail.com>
2018-09-02 01:26:40 -0700

    first working version

---
+++ README.md
@@ -0,0 +1,5 @@
+# BoxCoxTrans.jl
+
+This package provides an implementation of Box Cox transformation.
+
+Requires Julia 0.7/1.0.
--- src/BoxCoxTrans.jl
+++ src/BoxCoxTrans.jl
@@ -3,22 +3,38 @@ module BoxCoxTrans
 export transform
 
 using Optim: optimize, minimizer
+using Statistics: mean
 
-function log_likelihood_function(𝐱, λ)
-    𝐲 = boxcox(float.(𝐱), λ)
+"""
+Transform an array using Box-Cox method.  The lambda parameter is derived
+using a maximum likelihood estimator.  
+"""
+transform(𝐱) = transform(𝐱, lambda(𝐱))
+
+"""
+Transform an array using Box-Cox method with the provided λ parameter. 
+"""
+transform(𝐱, λ) = @. λ ≈ 0 ? log(𝐱) : (𝐱 ^ λ - 1) / λ
+
+"""
+Calculate lambda parameter
+"""
+function lambda(𝐱; interval = (-2.0, 2.0))
+    i1, i2 = interval
+    res = optimize(λ -> -mle(𝐱, λ), i1, i2)
+    return minimizer(res)
+end
+
+"""
+Maximum Likelihood Estimator
+"""
+function mle(𝐱, λ)
+    𝐲 = transform(float.(𝐱), λ)
     μ = mean(𝐲)
     N = length(𝐱)
     llf = (λ - 1) * sum(log.(𝐱))
     llf -= N / 2.0 * log(sum((𝐲 .- μ) .^ 2) / N)
     return llf
-end
-
-boxcox(𝐱, λ) = λ ≈ 0 ? log.(𝐱) : (𝐱 .^ λ .- 1) ./ λ
-
-function transform(𝐱; interval = (-0.2, 0.2))
-    i1, i2 = interval
-    λ = optimize(λ -> -log_likelihood_function(𝐱, λ), i1, i2)
-    return boxcox(𝐱, λ)
 end
 
 end # module
